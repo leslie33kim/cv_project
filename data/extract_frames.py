80f6b989880ede02f6cf2ed2ff7527f3e497e3da
leslie33kim <67294700+leslie33kim@users.noreply.github.com>
2023-12-08 16:33:11 -0800

    add data

---
+++ data/extract_frames.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+#set fps rate 
+def extract_frames(video_path, output_folder, fps=10):
+    # Extract the video name without extension
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    # Create a subdirectory for each video(frame#_video-name)
+    output_folder_video = os.path.join(output_folder, f'frames_{video_name}')
+    print(f"Creating directory: {output_folder_video}")
+    # Create an 'images' subdirectory inside each video folder
+    output_folder_images = os.path.join(output_folder_video, 'images')
+    print(f"Creating directory: {output_folder_images}")
+    os.makedirs(output_folder_images, exist_ok=True)
+    # Define the output pattern for frames
+    output_pattern = os.path.join(output_folder_images, 'frame_%04d.jpg')
+
+    # FFmpeg command to extract frames with specified fps
+    command = [
+        'ffmpeg',
+        '-i', video_path,
+        '-vf', f'fps={fps}',
+        output_pattern
+    ]
+
+    # Run the FFmpeg command
+    subprocess.run(command)
+
+def process_videos(input_folder, output_folder, fps=10):
+    if not os.path.exists(input_folder):
+        print(f"Error: Input directory '{input_folder}' does not exist.")
+        return
+
+    # Iterate through each file in the input folder
+    for filename in os.listdir(input_folder):
+        if filename.endswith(('.mp4')):
+            video_path = os.path.join(input_folder, filename)
+            # Call the extract_frames function for each video
+            extract_frames(video_path, output_folder, fps)
+
+if __name__ == "__main__":
+    input_folder = 'input_vids'
+    output_folder = 'pre_trained'
+    fps = 10  # Adjust fps rate
+    process_videos(input_folder, output_folder, fps)
